673859e2f46f2fc66ffc13d4d8f04019b7ce990a
Howard Kim <howardkim820@gmail.com>
2018-06-27 22:05:33 -0700

    addedStats

--- app.js
+++ app.js
@@ -4,61 +4,92 @@ var imageArray = ['dva.png', 'genji.png', 'hanzo.png', 'junkrat.png', 'mercy.png
 // var imageArray = ['dva.png', 'genji.png', 'reaper.png']
 var first_card_clicked = null;
 var second_card_clicked = null;
-var total_possible_matches = 2;
+var total_possible_matches = 9;
 var match_counter = 0;
+var matches = 0;
+var attempts = 0;
+var accuracy = 0;
+var times_played = 0;
 
+function display_stats(){
+    var gamesPlayedDiv = $('.timesPlayed p');
+    gamesPlayedDiv.text(`# of games played ${times_played}`);
+    var numberOfTriesDiv = $('.numberOfTries');
+    numberOfTriesDiv.text(`number of tries: ${attempts}`);
+    var accuracyDiv = $('.accuracy');
+    accuracyDiv.text(`Accuracy: ${accuracy}%`);
+};
+
+function reset_stats(){
+    accuracy = 0;
+    matches = 0;
+    attempts = 0;
+    match_counter = 0;
+    display_stats();
+
+
+};
+
+function resetGame(){
+    times_played++;
+    // console.log('times played = ' + times_played);
+    $('.timesPlayed').text(`# of games played: ${times_played}`);
+    first_card_clicked = null;
+    second_card_clicked = null;
+    $('.card div').removeClass('showCharactersFace');
+    reset_stats();
+
+}
 function doThisWhenReady(){
     var arrayWithDoublePictures = doubleImage(imageArray);
     var randomArray = randomizer(arrayWithDoublePictures)
     appendImages(randomArray);
     eventHandler();
+    display_stats();
 }
 function eventHandler(){
     $('.card').on('click', whenACardIsClicked)
+    $('.resetButton').on('click', resetGame);
 };
 function flipCardsBack(){
-    $(first_card_clicked).find(".front").removeClass('hidden');
-    $(second_card_clicked).find(".front").removeClass('hidden');
+    $(first_card_clicked).find(".back").removeClass('showCharactersFace');
+    $(second_card_clicked).find(".back").removeClass('showCharactersFace');
     first_card_clicked = null;
     second_card_clicked = null;
-    // $('.card').on('click', whenTheFrontCardIsClicked);
 }
 function whenACardIsClicked() {
-
-    //if the character card is revealed and the logo becomes hidden, break out of the function
-    if ($(this).find('.front').hasClass('hidden')) { 
-        console.log('hit')
+    if ($(this).find('.back').hasClass('showCharactersFace')) {
+        // console.log('you have been clicked')
         return;
     }
-    // if (first_card_clicked == this) { //prevents you from 'using' the same card more than once
-    //     console.log("you already picked this card as your first card");
-    //     return;
-    // }
-
     if (first_card_clicked !== null && second_card_clicked !== null){
         return;
     }
     if(first_card_clicked === null){
         first_card_clicked = this;
-        $(first_card_clicked).find(".front").addClass('hidden');
+
         
+        $(first_card_clicked).find(".back").addClass('showCharactersFace');
+        return;
     } else { 
         second_card_clicked = this;
-        // $('.card').off('click');
-        $(second_card_clicked).find(".front").addClass('hidden');
-        var firstCardImageURL = $(first_card_clicked).find('.back').css('background-image');
-        var secondCardImageURL = $(second_card_clicked).find('.back').css('background-image');
+
+
+        $(second_card_clicked).find(".back").addClass('showCharactersFace');
+        var firstCardImageURL = $(first_card_clicked).find('.front').css('background-image');
+        var secondCardImageURL = $(second_card_clicked).find('.front').css('background-image');
         if(firstCardImageURL === secondCardImageURL){ // if the cards ARE A MATCH
+            matches++;
             match_counter++;
             first_card_clicked = null;
             second_card_clicked = null;
         } else { // if the cards are NOT a match
             setTimeout(flipCardsBack, 2000);
-            // first_card_clicked = null;
-            // second_card_clicked = null;
+            attempts++;
         }
-
     }
+    accuracy = (matches/attempts).toFixed(2) * 100;
+    display_stats();
 };
 
 function appendImages(array){
@@ -67,31 +98,17 @@ function appendImages(array){
             class: 'card'
         });
         var divWithFrontClass = $('<div>', {
-            class: 'front',
+            class: 'back',
             attr: {
               style: `background-image: url('images/frontcard.jpg')`
             }
         });
         var divWithBackClass = $('<div>', {
-            class: 'back' ,
+            class: 'front' ,
             attr: {
                 style: `background-image: url('images/${array[i]}')`
             }
         });
-        // var imageElement = $('<img>', {
-        //     attr: {
-        //         src: `./images/${array[i]}`
-        //     }
-        // });
-
-        // var frontImageElement = $('<img>' , {
-        //     attr: {
-        //         src: `./images/${frontCardImage[0]}`
-        //     }
-        // });
-
-        // divWithBackClass.append(imageElement);
-        // divWithFrontClass.append(frontImageElement)
         newDiv.append(divWithFrontClass, divWithBackClass);
         $('.rowOne').append(newDiv);
     };
@@ -107,12 +124,9 @@ function doubleImage(array){
 function randomizer(array){
     var randomArray = [];
     var copiedArray = array.slice(0);
-    // debugger;
-    // << this line makes a copy of the array without affecting the original array
     while(copiedArray.length > 0){
         var randomIndex = Math.floor(Math.random() * copiedArray.length);
         randomArray.push(copiedArray.splice(randomIndex, 1)[0]);
-        //splice affects the original array and returns you an item in AN ARRAY!!!
     }
     return randomArray
 }
